e4a9c002f942e1fbdc010ce241bbacae6927ef23
John Minns <john.minns@dazn.com>
2022-08-31 18:04:03 +0100

    chore: finish material

--- src/components/form/inputs/TextInput/TextInput.material.tsx
+++ src/components/form/inputs/TextInput/TextInput.material.tsx
@@ -1,7 +1,35 @@
-import TextField from '@mui/material/TextField';
+import { styled } from '@mui/material';
+import TextField, { TextFieldProps } from '@mui/material/TextField';
+
+const StyledInput = styled(TextField)<TextFieldProps>({
+  display: 'block',
+  '& .MuiInputBase-root': {
+    width: '100%',
+    padding: '10px',
+    cursor: 'text',
+    fontFamily: 'Arial, Helvetica, sans-serif',
+    fontSize: '13px',
+    lineHeight: 1,
+    borderRadius: '0px',
+
+    display: 'flex',
+    '&:after': {
+      borderBottom: '2px solid #ced0d2',
+      transform: 'unset',
+      transition: 'unset',
+    },
+  },
+  '& .MuiInputBase-root.Mui-focused': {
+    '&:after': {
+      borderBottom: '2px solid #0c161c',
+      transform: 'unset',
+      transition: 'unset',
+    },
+  }
+}) as typeof TextField;
 
 export const TextInput: React.FC = (props) => {
   return (
-      <TextField {...props} />
+      <StyledInput {...props} variant="standard" />
   );
 }
